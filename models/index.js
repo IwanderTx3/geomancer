32d51ae4825e2bf71123f244c341f35027082c51
Van Collins <v3development@Vans-MacBook-Pro.local>
2018-09-11 15:21:35 -0500

    testing live server

--- models/index.js
+++ models/index.js
@@ -4,16 +4,18 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
+//var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.NODE_ENV || 'test';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 console.log(config)
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-//    var sequelize = new Sequelize('postgres://localhost:5433/geomancer',config)
+ var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// var sequelize = new Sequelize('postgres://localhost:5433/geomancer',config);
+//var sequelize = new Sequelize('postgres://geomancer.clw53lchrbyq.us-east-1.rds.amazonaws.com',config);
 } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
-//var sequelize = new Sequelize('postgres://localhost:5433/geomancer',config)
+    //var sequelize = new Sequelize('postgres://geomancer.clw53lchrbyq.us-east-1.rds.amazonaws.com',config)
 }
 
 fs
